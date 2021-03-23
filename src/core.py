cb7aed14dbcbfbaac2eab0ccd0de54ef463ddc36
TrollSkull <64570084+TrollSkull@users.noreply.github.com>
2021-03-23 15:07:28 -0300

    Add files via upload

---
+++ src/core.py
@@ -0,0 +1,34 @@
+import youtube_dl
+
+ydl_opts = {}
+
+def dwl_vid():
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([zxt])
+
+option = 1
+channel = 1
+more = ""
+
+while(option == int(1)):
+    while(channel == int(1)):
+        print("") # Space
+        link_of_video = input("Copy & Paste the URL of the video: ")
+        zxt = link_of_video.strip()
+
+        print("") # Space
+        
+        dwl_vid()
+
+        print("") # Space
+        more = str(input("Do you want to download more videos? (y/n): "))
+        if more == "y":
+            option = 0
+            channel = 1
+        elif more == "n":
+            option = 0
+            channel = 0
+        else:
+            option = 1
+            print("") # Space
+            print ("There is no such option, please enter a valid option.")
---
+++ src/options/about.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+R='\033[31m'
+W='\033[37m'
+
+echo -e ${R}"Author:    ${W}TrollSkull"
+echo -e ${R}"Version:   ${W}1.0"
+echo -e ${R}"Name:      ${W}BashTube"
+echo -e ${R}"Contact:   ${W}trollskull.contact@gmail.com"
+echo
+echo -e ${R}"If you find errors; Please contact me.${W}"
+echo
