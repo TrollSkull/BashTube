90be4d6a0b6db0cd80ec044a57649b073e5787f6
TrollSkull <64570084+TrollSkull@users.noreply.github.com>
2021-03-25 03:57:20 -0300

    Add files via upload

---
+++ bashtube.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+# Tool Name: BashTube
+# Author: TrollSkull
+# Date: 23/03/2021
+
+R='\033[31m'
+GRE='\033[32m'
+W='\033[37m'
+B='\033[34m'
+GRA='\033[1;30m'
+Y='\033[1;33m'
+C='\033[1;36m'
+
+clear
+
+function banner () {
+        echo -e ${R}""
+        cd src
+        cat banner.txt
+        cd ..
+        echo
+        echo -e ${R}" BashTube: ${W}https://github.com/TrollSkull/BashTube"
+        echo
+}
+
+function call_banner () {
+        clear
+        banner
+}
+
+function mv_videos () {
+        mv *.mp4 /data/data/com.termux/files/home/storage/downloads
+}
+
+call_banner
+
+start="1" # Exit switch
+while [ "$start" = "1" ]
+do
+    
+    read -p "BashTube >> " entry
+
+    if [ "$entry" == "help" ]; then
+            call_banner
+
+	    cd src
+            cd options
+	    bash com.sh
+	    cd ..
+            cd ..
+
+    elif [ "$entry" == "convert" ]; then
+            call_banner
+
+            cd src
+            python core.py
+
+            mv_videos
+
+	    cd ..
+
+            call_banner
+
+    elif [ "$entry" == "update" ]; then
+
+            call_banner
+
+	    echo -e ${Y}"[!] ${W}It will be updated to the most recent version of the tool, if"
+	    echo "you already have the latest version, it will be downloaded again."
+            echo
+
+            read -p "[?] Do you wanna update the tool? (y/n): " update
+            echo
+
+            if [ "$update" == "y" ]||[ "$update" == "Y" ]; then
+                echo -e ${B}"[*] ${W}Updating..."
+                
+                cd ..
+                rm -rf BashTube
+
+                git clone http://github.com/TrollSkull/BashTube &> /dev/null
+                cd BashTube
+                bash installer.sh
+
+                exit
+
+            elif [ "$update" == "n" ]||[ "$update" == "N" ]; then
+                echo -e ${Y}"[-] ${W}Update canceled."
+                sleep 2
+
+                call_banner
+
+            else
+                echo -e ${R}"[!] ${W}That option does not exist."
+                sleep 2
+
+                call_banner
+
+            fi
+        
+    elif [ "$entry" == "about" ]; then
+            call_banner
+
+            cd src
+            cd options
+            bash about.sh
+            cd ..
+            cd ..
+
+    elif [ "$entry" == "exit" ]; then
+            echo
+            echo -e ${W}"Thanks for using ${R}BashTube${W}, I hope you enjoyed this tool. :)"
+
+            sleep 3
+            clear
+            exit
+
+    else
+            echo
+            echo -e ${R}"[!] ${W}Command not found, type ${R}help ${W}for commands."
+            sleep 2
+
+            call_banner
+    fi 
+
+done
---
+++ installer.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Tool Name: BashTube (Installer)
+# Author: TrollSkull
+# Date: 23/03/2021
+
+R='\033[31m'
+GRE='\033[32m'
+W='\033[37m'
+B='\033[34m'
+GRA='\033[1;30m'
+Y='\033[1;33m'
+C='\033[1;36m'
+
+clear
+rm -rf README.md
+rm -rf LICENSE
+rm -rf installer.sh
+
+echo -e ${B}"[*] ${W}Storage access required for downloaded videos."
+
+termux-setup-storage
+clear
+
+function banner () {
+        echo -e ${R}""
+        cd src
+        cat banner.txt
+        cd ..
+        echo
+        echo -e ${R}" BashTube: ${W}https://github.com/TrollSkull/BashTube"
+        echo
+}
+
+banner
+
+echo -e ${B}"[*] ${W}Updating Git..."
+pkg install -y git &> /dev/null
+
+echo -e ${GRE}"[+] ${W}Done!"
+
+echo -e ${B}"[*] ${W}Updating Python..."
+pkg install -y python &> /dev/null
+pkg install -y python2 &> /dev/null
+python -m pip install --upgrade pip &> /dev/null
+
+echo -e ${GRE}"[+] ${W}Done!"
+
+echo -e ${B}"[*] ${W}Downloading resources..."
+pip install youtube_dl &> /dev/null
+
+echo -e ${GRE}"[+] ${W}Done!"
+
+sleep 2
+
+clear
+banner
+
+echo -e "      Type ${R}$ bash bashtube.sh ${W}to run BashTube."
+echo
+
+sleep 4
+exit
