2500f1a5a90e98823bcba53cac6fde71cc058281
TrollSkull <64570084+TrollSkull@users.noreply.github.com>
2021-03-28 01:53:38 +0100

    Update about.sh

--- src/options/about.sh
+++ src/options/about.sh
@@ -4,9 +4,9 @@ R='\033[31m'
 W='\033[37m'
 
 echo -e ${R}"Author:    ${W}TrollSkull"
-echo -e ${R}"Version:   ${W}1.0"
+echo -e ${R}"Version:   ${W}1.1"
 echo -e ${R}"Name:      ${W}BashTube"
 echo -e ${R}"Contact:   ${W}trollskull.contact@gmail.com"
 echo
-echo -e ${R}"If you find errors, please contact me.${W}"
+echo -e ${R}"If you find errors, please contact me${W}"
 echo
