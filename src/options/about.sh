a4c764bcd70cc3b1e1bb432a0202e1c87bf94a59
TrollSkull <leanahuelmiranda@gmail.com>
2021-03-31 22:48:34 +0200

    Update about.sh

--- src/options/about.sh
+++ src/options/about.sh
@@ -4,7 +4,7 @@ R='\033[31m'
 W='\033[37m'
 
 echo -e ${R}"Author:    ${W}TrollSkull"
-echo -e ${R}"Version:   ${W}1.3.0"
+echo -e ${R}"Version:   ${W}1.3"
 echo -e ${R}"Name:      ${W}BashTube"
 echo
 echo -e ${R}"Contact:   ${W}trollskull.contact@gmail.com"
