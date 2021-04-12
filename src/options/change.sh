6480606d4c82c8c6c03e9eebc92f595bf5b2b047
TrollSkull <leanahuelmiranda@gmail.com>
2021-04-12 04:40:53 +0200

    Update change.sh

--- src/options/change.sh
+++ src/options/change.sh
@@ -8,16 +8,16 @@ GRA='\033[1;30m'
 Y='\033[1;33m'
 C='\033[1;36m'
 
-echo -e ${R}"Version 1.0.0${W}"
+echo -e ${R}"Version 1.0${W}"
 echo "- Initial version, no changes"
 echo
-echo -e ${R}"Version 1.1.0${W}"
+echo -e ${R}"Version 1.1${W}"
 echo "- Updater changes"
 echo "- Added 'Change log' option"
 echo "- Changes in commands"
 echo "- Minor bug fixes"
 echo
-echo -e ${R}"Version 1.2.0${W}"
+echo -e ${R}"Version 1.2${W}"
 echo "- Now you can download images"
 echo "- Implemented bug exceptions"
 echo "- Changes to the image download system"
