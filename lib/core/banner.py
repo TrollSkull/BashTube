06ed27d819f16980e93a9c7fae540502b6a8b989
TrollSkull <trollskull.contact@gmail.com>
2021-12-23 21:09:16 +0100

    Update banner.py

--- lib/core/banner.py
+++ lib/core/banner.py
@@ -5,20 +5,23 @@ class Colors:
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     RESET = '\033[0m'
+    WORKING = '\033[34m'
 
 def PrintBanner():
     if sys.platform == "win32":
         os.system("cls")
-        print(Colors.FAIL + "   ___           __ ______     __            ")
-        print("  / _ )___ ____ / //_  __/_ __/ /  ___         ")
-        print(" / _  / _ `(_-</ _ \/ / / // / _ \/ -_)        ")
-        print("/____/\_,_/___/_//_/_/  \_,_/_.__/\__/   v1.4 A")
+        print(Colors.WORKING + "   ___           __" + Colors.FAIL + "  ______     __            ")
+        print(Colors.WORKING + "  / _ )___ ____ / /" + Colors.FAIL + " /_  __/_ __/ /  ___       ")
+        print(Colors.WORKING + " / _  / _ `(_-</ _\ " + Colors.FAIL + " / / / // / _ \/ -_)      ")
+        print(Colors.WORKING + "/____/\_,_/___/_//_" + Colors.FAIL + " /_/  \_,_/_.__/\__/   " + Colors.RESET + "v2.0 A")
         print(Colors.RESET)     
+        print("[i] Press CTRL+C to stop the script.")
 
     else:
         os.system("clear")
-        print(Colors.FAIL + "   ___           __ ______     __            ")
-        print("  / _ )___ ____ / //_  __/_ __/ /  ___         ")
-        print(" / _  / _ `(_-</ _ \/ / / // / _ \/ -_)        ")
-        print("/____/\_,_/___/_//_/_/  \_,_/_.__/\__/   v1.4 A")
-        print(Colors.RESET)     
+        print(Colors.WORKING + "   ___           __" + Colors.FAIL + "  ______     __            ")
+        print(Colors.WORKING + "  / _ )___ ____ / /" + Colors.FAIL + " /_  __/_ __/ /  ___       ")
+        print(Colors.WORKING + " / _  / _ `(_-</ _\ " + Colors.FAIL + " / / / // / _ \/ -_)      ")
+        print(Colors.WORKING + "/____/\_,_/___/_//_" + Colors.FAIL + " /_/  \_,_/_.__/\__/   " + Colors.RESET + "v2.0 A")
+        print(Colors.RESET) 
+        print("[i] Press CTRL+C to stop the script.")   
